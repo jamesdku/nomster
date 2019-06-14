0cd4f09f92d1f24c69992455d27bccace09313b9
James McJunkin <jmcj88@gmail.com>
2019-06-14 19:04:36 +0000

    new migrate

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,16 @@ ActiveRecord::Schema.define(version: 2019_06_13_204428) do
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
