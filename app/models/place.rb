1560bde5c0861901967a8d6a39da485054a15937
James McJunkin <jmcj88@gmail.com>
2019-07-04 00:54:10 +0000

    MV

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,5 +4,7 @@ class Place < ApplicationRecord
   has_many :photos
   geocoded_by :address
   after_validation :geocode
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
